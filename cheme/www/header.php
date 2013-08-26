65980e3e601f46849d4e4d14d7322bf63788b934
Mark Laney <markjlaney@gmail.com>
2013-08-26 15:33:02 -0600

    Made small change to how name shows up in index.php after logging in.

--- cheme/www/header.php
+++ cheme/www/header.php
@@ -25,7 +25,7 @@
 
 				if(isset($_COOKIE["username"])){
 				
-					$value = "Welcome " . $_COOKIE["username"] . ".";
+					$value = "Welcome " . $_COOKIE["username"];
 					$set = true;
 				}
 				else{
