664513ad5617ac9130e2176a5f193754c34fff44
Mark Laney <markjlaney@gmail.com>
2013-09-04 13:22:04 -0600

    made final change to abet.php

--- cheme/www/abetsidebar.php
+++ cheme/www/abetsidebar.php
@@ -7,7 +7,7 @@
 		<ul style="list-style-type:none">
 		<li><a href="courseobjectives.php">Competencies</a></li>
 		<li><a href="https://learningoutcomes.byu.edu">Educational Objectives</a></li>
-		<li><a href="https://learningoutcomes.byu.edu">Direct Assessemnt Measurements</a></li>
+		<li><a href="https://learningoutcomes.byu.edu">Direct Assessment Measurements</a></li>
 		<li><a href="https://learningoutcomes.byu.edu">Indirect Assessment Measurements</a></li>
 		<li><a href="https://learningoutcomes.byu.edu">Student Outcomes</a></li>
 		<li><a href="History.php">Department Mission</a></li>
